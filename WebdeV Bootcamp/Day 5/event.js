d2a0db44fd021fba0ec9fdf3dd2844df9f4088a1
Adityakarmakar1 <aditya.karmkr@gmail.com>
2024-03-24 22:13:04 +0530

    Updated the code

--- WebdeV Bootcamp/Day 5/event.js
+++ WebdeV Bootcamp/Day 5/event.js
@@ -2,4 +2,17 @@ function handleClick(){
     alert("Button Clicked")
 }
 
+//recommended way
 document.getElementById("clickme").addEventListener("click", handleClick)
+
+function handlemouseOver(event){
+    console.log(event)
+}
+
+document.getElementById("start").addEventListener("click", function handleClick(){
+    document.addEventListener("mouseover", handlemouseOver)
+})
+
+document.addEventListener("keyup", function(e){
+    console.log(e.key);
+})
